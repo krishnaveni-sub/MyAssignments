5c189edb295fcf56ace74fcf1524ee729ff2ace0
krishnaveni-sub <krishnavenisubramoniam@gmail.com>
2025-06-22 08:58:38 +0530

    Add files via upload
    Day 1 - Home assignment

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+	    String developer ="Google";
+		boolean  isBeta =false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Browser version ="+version +"," +"Browser developer = "+ developer+ " ,"
+		+ " Beta  version =" +isBeta+" ,"+"Release of year = " +releaseYear+ ","
+				+"Shortcut key = "+shortcutKey);
+	}
+
+}
